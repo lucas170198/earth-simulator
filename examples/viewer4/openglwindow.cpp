47287d98a5359356a62e2a2aac0687e0e7b5ac90
lucas170198 <lucassilvaamorim1998@gmail.com>
2021-12-07 20:20:44 -0300

    Simple earth model

---
+++ examples/viewer4/openglwindow.cpp
@@ -0,0 +1,165 @@
+#include "openglwindow.hpp"
+
+#include <imgui.h>
+
+#include <cppitertools/itertools.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+
+#include "imfilebrowser.h"
+
+void OpenGLWindow::handleEvent(SDL_Event& event) {
+  glm::ivec2 mousePosition;
+  SDL_GetMouseState(&mousePosition.x, &mousePosition.y);
+
+  if (event.type == SDL_MOUSEMOTION) {
+    m_trackBallModel.mouseMove(mousePosition);
+    m_trackBallLight.mouseMove(mousePosition);
+  }
+  if (event.type == SDL_MOUSEBUTTONDOWN) {
+    if (event.button.button == SDL_BUTTON_LEFT) {
+      m_trackBallModel.mousePress(mousePosition);
+    }
+    if (event.button.button == SDL_BUTTON_RIGHT) {
+      m_trackBallLight.mousePress(mousePosition);
+    }
+  }
+  if (event.type == SDL_MOUSEBUTTONUP) {
+    if (event.button.button == SDL_BUTTON_LEFT) {
+      m_trackBallModel.mouseRelease(mousePosition);
+    }
+    if (event.button.button == SDL_BUTTON_RIGHT) {
+      m_trackBallLight.mouseRelease(mousePosition);
+    }
+  }
+  if (event.type == SDL_MOUSEWHEEL) {
+    m_zoom += (event.wheel.y > 0 ? 1.0f : -1.0f) / 5.0f;
+    m_zoom = glm::clamp(m_zoom, -1.5f, 1.0f);
+  }
+}
+
+void OpenGLWindow::initializeGL() {
+  abcg::glClearColor(0, 0, 0, 1);
+  abcg::glEnable(GL_DEPTH_TEST);
+
+  // Create programs
+  for (const auto& name : m_shaderNames) {
+    const auto path{getAssetsPath() + "shaders/" + name};
+    const auto program{createProgramFromFile(path + ".vert", path + ".frag")};
+    m_programs.push_back(program);
+  }
+
+  // Load default model
+  loadModel(getAssetsPath() + "world.obj");
+  // Initial trackball spin
+  m_trackBallModel.setAxis(glm::normalize(glm::vec3(1, 1, 1)));
+  m_trackBallModel.setVelocity(0.0001f);
+}
+
+void OpenGLWindow::loadModel(std::string_view path) {
+  m_model.terminateGL();
+
+  m_model.loadDiffuseTexture(getAssetsPath() + "maps/world_giant.jpg");
+  m_model.loadObj(path);
+  m_model.setupVAO(m_programs.at(m_currentProgramIndex));
+
+  // Use material properties from the loaded model
+  m_Ka = m_model.getKa();
+  m_Kd = m_model.getKd();
+  m_Ks = m_model.getKs();
+  m_shininess = m_model.getShininess();
+}
+
+void OpenGLWindow::paintGL() {
+  update();
+
+  abcg::glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  abcg::glViewport(0, 0, m_viewportWidth, m_viewportHeight);
+
+  // Use currently selected program
+  const auto program{m_programs.at(m_currentProgramIndex)};
+  abcg::glUseProgram(program);
+
+  // Get location of uniform variables
+  const GLint viewMatrixLoc{abcg::glGetUniformLocation(program, "viewMatrix")};
+  const GLint projMatrixLoc{abcg::glGetUniformLocation(program, "projMatrix")};
+  const GLint modelMatrixLoc{
+      abcg::glGetUniformLocation(program, "modelMatrix")};
+  const GLint normalMatrixLoc{
+      abcg::glGetUniformLocation(program, "normalMatrix")};
+  const GLint lightDirLoc{
+      abcg::glGetUniformLocation(program, "lightDirWorldSpace")};
+  const GLint shininessLoc{abcg::glGetUniformLocation(program, "shininess")};
+  const GLint IaLoc{abcg::glGetUniformLocation(program, "Ia")};
+  const GLint IdLoc{abcg::glGetUniformLocation(program, "Id")};
+  const GLint IsLoc{abcg::glGetUniformLocation(program, "Is")};
+  const GLint KaLoc{abcg::glGetUniformLocation(program, "Ka")};
+  const GLint KdLoc{abcg::glGetUniformLocation(program, "Kd")};
+  const GLint KsLoc{abcg::glGetUniformLocation(program, "Ks")};
+  const GLint diffuseTexLoc{abcg::glGetUniformLocation(program, "diffuseTex")};
+  const GLint mappingModeLoc{
+      abcg::glGetUniformLocation(program, "mappingMode")};
+
+  // Set uniform variables used by every scene object
+  abcg::glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, &m_viewMatrix[0][0]);
+  abcg::glUniformMatrix4fv(projMatrixLoc, 1, GL_FALSE, &m_projMatrix[0][0]);
+  abcg::glUniform1i(diffuseTexLoc, 0);
+  abcg::glUniform1i(mappingModeLoc, m_mappingMode);
+
+  const auto lightDirRotated{m_trackBallLight.getRotation() * m_lightDir};
+  abcg::glUniform4fv(lightDirLoc, 1, &lightDirRotated.x);
+  abcg::glUniform4fv(IaLoc, 1, &m_Ia.x);
+  abcg::glUniform4fv(IdLoc, 1, &m_Id.x);
+  abcg::glUniform4fv(IsLoc, 1, &m_Is.x);
+
+  // Set uniform variables of the current object
+  abcg::glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, &m_modelMatrix[0][0]);
+
+  const auto modelViewMatrix{glm::mat3(m_viewMatrix * m_modelMatrix)};
+  glm::mat3 normalMatrix{glm::inverseTranspose(modelViewMatrix)};
+  abcg::glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, &normalMatrix[0][0]);
+
+  abcg::glUniform1f(shininessLoc, m_shininess);
+  abcg::glUniform4fv(KaLoc, 1, &m_Ka.x);
+  abcg::glUniform4fv(KdLoc, 1, &m_Kd.x);
+  abcg::glUniform4fv(KsLoc, 1, &m_Ks.x);
+
+  m_model.render();
+
+  abcg::glFrontFace(GL_CCW);
+  abcg::glDisable(GL_CULL_FACE);
+
+  const auto aspect{static_cast<float>(m_viewportWidth) /
+                  static_cast<float>(m_viewportHeight)};
+
+  m_projMatrix =
+    glm::perspective(glm::radians(45.0f), aspect, 0.1f, 5.0f);
+
+  abcg::glUseProgram(0);
+}
+
+void OpenGLWindow::paintUI() {
+  abcg::OpenGLWindow::paintUI();
+}
+
+void OpenGLWindow::resizeGL(int width, int height) {
+  m_viewportWidth = width;
+  m_viewportHeight = height;
+
+  m_trackBallModel.resizeViewport(width, height);
+  m_trackBallLight.resizeViewport(width, height);
+}
+
+void OpenGLWindow::terminateGL() {
+  m_model.terminateGL();
+  for (const auto& program : m_programs) {
+    abcg::glDeleteProgram(program);
+  }
+}
+
+void OpenGLWindow::update() {
+  m_modelMatrix = m_trackBallModel.getRotation();
+
+  m_viewMatrix =
+      glm::lookAt(glm::vec3(0.0f, 0.0f, 2.0f + m_zoom),
+                  glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+}
